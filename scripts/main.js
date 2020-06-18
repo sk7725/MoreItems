3a6e080b78053ff37e044d9341003ca97033cb08
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-06-18 13:33:06 +0900

    ...yup

--- scripts/main.js
+++ scripts/main.js
@@ -93,12 +93,10 @@ function addItemForm(pitem, type, form, brightOffset){
   print("Add item:"+item);
 }
 
-Events.on(EventType.ClientLoadEvent, run(() => {
-  print("Init!");
-  Vars.content.items().each(cons(it=>{
-    print("Iter:"+it);
-    if(it.name.substring(0,9)=="moreitems") return;
-    addItemForm(it, "resource", "pieces", -0.04);
-    addItemForm(it, "material", "rod", 0.04);
-  }));
+print("Init!");
+Vars.content.items().each(cons(it=>{
+  print("Iter:"+it);
+  if(it.name.substring(0,9)=="moreitems") return;
+  addItemForm(it, "resource", "pieces", -0.04);
+  addItemForm(it, "material", "rod", 0.04);
 }));
