96e50ac09d67d2aaa46030925884535e779ee615
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-06-19 22:36:13 +0900

    Update props.js

--- scripts/props.js
+++ scripts/props.js
@@ -29,8 +29,8 @@ this.global.MoreItems.itemprops.flammable = [];
 this.global.MoreItems.itemprops.radioactive = ["cesium","cobalt","iodine","plutonium","radium","radon","strontium","thorium","uranium","radioactive"];
 
 this.global.MoreItems.itemprops.energy = ["pyratite","cell","power","energy"];
-this.global.MoreItems.itemprops.data = ["surge","quantum","scalar","vector","tensor","intelli","byte","bittrium","source","code","terminal","hash","heap","binary","variable","constant","advance","pixellium","zeta","redblack"];
-this.global.MoreItems.itemprops.ohno = ["ohno","anuke","sk7725"];
+this.global.MoreItems.itemprops.data = ["surge","quantum","scalar","vector","tensor","intelli","byte","bittrium","source","code","terminal","hash","heap","binary","variable","constant","advance","pixellium","zeta","redblack","infiar","matrix"];
+this.global.MoreItems.itemprops.ohno = ["ohno","anuke","sk7725","test"];
 this.global.MoreItems.itemprops.trash = ["scrap","itemform-pieces"];
 
 this.global.MoreItems.itemprops.processed = ["rod","chiseled","brick","pellet","itemform-dust"];
